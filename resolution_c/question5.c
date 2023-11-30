4fd4810de1fe2b4c0046a818ba97df01433170df
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-11-30 18:23:40 -0300

    Update question5.c

--- resolution_c/question5.c
+++ resolution_c/question5.c
@@ -8,13 +8,14 @@ int main ( ) {
 	for ( i =1 ; i <= 100 ; i *=100) {
 		if ( i==30 ) {
 			break ;
-			}
+		}
 		else {
 			printf ("%2d\n" ,2* i ) ;
-			}
 		}
-		printf ("Fim do laco !\n") ;
-		return 0;
+		
+	}
+	printf ("Fim do laco !\n") ;
+	return 0;
 }
 
 
