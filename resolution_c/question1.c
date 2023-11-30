78e47f3cf7c62c0f6eac063073a3daab1cfa538d
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-11-30 18:13:39 -0300

    Update question1.c

--- resolution_c/question1.c
+++ resolution_c/question1.c
@@ -10,11 +10,10 @@ int main() {
 	}
 	
 	for (i=0;i<10;i++){
-	printf("%d", vet[i]);
+		printf("%d", vet[i]);
 	}
 
 printf("\n\n");
-
 
 	int i=0; // zera a variavel i
 
@@ -26,7 +25,7 @@ printf("\n\n");
 	}
 
 	for(i=0;i<10;i++){
-	printf("%d", vet[i]);
+		printf("%d", vet[i]);
 	}
 
 return 0;
