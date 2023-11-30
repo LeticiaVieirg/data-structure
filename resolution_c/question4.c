e33bdbf27d8fb4fb14cbdbd2418af72add1694c5
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-11-30 07:31:36 -0300

    Update question4.c

--- resolution_c/question4.c
+++ resolution_c/question4.c
@@ -1,1 +1,25 @@
 //
+#include <stdio.h>
+int main(){  
+
+    int soma(int a, int b){  
+        int soma = 0;if (a > b){
+        for (int i = b+1; i < b; i++){
+            soma += i;
+            }
+         }
+        else{
+           for (int i = a+1; i <b; i++){
+            soma += i;
+        }
+    }
+    return soma;
+}
+
+int main () {
+    int x = 5, y = 0;
+    int res= soma(x, y);
+    
+    printf("%d", res);
+    return 0;
+}
