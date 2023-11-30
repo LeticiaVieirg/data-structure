8eab30fa4329124f988545ff362a563cb3f7a459
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-11-29 21:41:57 -0300

    Update question1.c

--- resolution_c/question1.c
+++ resolution_c/question1.c
@@ -1,2 +1,33 @@
-//Implemente uma funçaoao que receba como parˆametro um vetor de n´umeros inteiros (vet) de tamanho
-//n e inverta a ordem dos elementos armazenados nesse vetor
+
+#include <stdio.h>
+
+int main() {
+	int vet[10];
+	int i,j,aux;
+
+	for(i=0;i<10;i++) {
+		vet[i]=i;
+	}
+	
+	for (i=0;i<10;i++){
+	printf("%d", vet[i]);
+	}
+
+printf("\n\n");
+
+
+	int i=0; // zera a variavel i
+
+	for(j=9;j>=4;j--) {
+		aux=vet[i];
+		vet[i]=vet[j];
+		vet[j]=aux;
+		i++;
+	}
+
+	for(i=0;i<10;i++){
+	printf("%d", vet[i]);
+	}
+
+return 0;
+}
