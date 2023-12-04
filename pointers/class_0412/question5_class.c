82fa2f82bac41dfcf5574b3c1df874dc30c66ead
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-12-04 19:12:41 -0300

    Create question5_class.c

---
+++ pointers/class_0412/question5_class.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int create_variable () {
+  int variable=5;
+  
+return &variable;
+}
+
+int main() {
+  int *pointer=create_variable();
+  printf("Value of variable: %d", *pointer);
+
+  return 0;
+}
